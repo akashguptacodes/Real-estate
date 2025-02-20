91be6eb1e36d3a6770a7d08bfcb55d3c6bfa7487
Akash <akashgupta7484@gmail.com>
2025-02-20 12:46:39 +0530

    socket url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        setSocket(io("http://localhost:5000"))
+        setSocket(io(import.meta.env.VITE_BACKEND_URL))
     }, []);
 
     useEffect(()=>{
